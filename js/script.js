e17336b83085180fe51a50b08fef7468604771af
justxwayz <ahuarii.fb@gmail.com>
2024-03-29 18:59:11 +0100

    update all files - 19h

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+document.querySelectorAll('.link-section').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.menu__item').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
